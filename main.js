53fbbc09dc9638078ed968fc45d733d1b18f178b
Islam Mashukov <maks.velikiy13@mail.ru>
2023-03-25 20:13:35 +0300

    2 commit

--- main.js
+++ main.js
@@ -34,25 +34,26 @@
 
 // + promise chain
 
-// function request(url) {  
-//     let promise = new Promise((resolve, reject) => {
-//       const req = new XMLHttpRequest();
+function request(url) {  
+    console.log(url);
+    let promise = new Promise((resolve, reject) => {
+      const req = new XMLHttpRequest();
       
-//       req.open('GET', url);
+      req.open('GET', url);
       
-//       req.onload = function() {
+      req.onload = function() {
         
-//         if (req.status === 200) {
-//           resolve(JSON.parse(req.response))
-//         } else {
-//           reject(Error(req.status));
-//         }
-//       }
+        if (req.status === 200) {
+          resolve(JSON.parse(req.response))
+        } else {
+          reject(Error(req.status));
+        }
+      }
     
-//       req.send();
-//     });
+      req.send();
+    });
     
-//     return promise;
-//   }
+    return promise;
+  }
 
-//   request()
+  request(url)
